775fbc11411c66ce1f828a8a9745270e91b2eee6
G2HUB <djilali.eljilali@gmail.com>
2021-02-21 10:08:04 +0100

    Initial Boot app creation closes #1

---
+++ src/main/java/guru/g2/recipeapp/G2RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.g2.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2RecipeAppApplication.class, args);
+	}
+
+}
