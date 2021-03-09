9efcf1ff2a0941147d04df931576c61408cf7058
G2HUB <djilali.eljilali@gmail.com>
2021-03-09 23:12:27 +0100

    test

--- src/test/java/guru/g2/recipeapp/G2RecipeAppApplicationTests.java
+++ src/test/java/guru/g2/recipeapp/G2RecipeAppApplicationTests.java
@@ -1,15 +1,12 @@
 package guru.g2.recipeapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@SpringBootTest
+//@SpringBootTest
 class G2RecipeAppApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 		log.info("Context load @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 		log.info("Context load @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
